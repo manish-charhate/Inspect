0f6124d6a7a31ffde94f373407052664e996622d
Manish Charhate <66906444+manish-charhate@users.noreply.github.com>
2024-08-04 19:08:57 +0530

    Initial Commit

---
+++ Inspect/InspectApp.swift
@@ -0,0 +1,17 @@
+//
+//  InspectApp.swift
+//  Inspect
+//
+//  Created by Manish Charhate on 04/08/24.
+//
+
+import SwiftUI
+
+@main
+struct InspectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
